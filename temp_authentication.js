3c71d271dd48140962488e56498dee6a08d19007
In joon. Gong <ijgong@nglp.kr>
2018-09-27 18:01:29 +0900

    facebook, naver 추가

--- temp_authentication.js
+++ temp_authentication.js
@@ -7,7 +7,9 @@ app.use(cors());
 
 // passport 설정
 var passport = require('passport'),
-    KakaoStrategy = require('passport-kakao').Strategy;
+    KakaoStrategy = require('passport-kakao').Strategy,
+    FacebookStrategy = require('passport-facebook').Strategy,
+    NaverStrategy = require('passport-naver').Strategy;
 
 // passport-kakao 설정
 passport.use(new KakaoStrategy({
@@ -21,6 +23,33 @@ passport.use(new KakaoStrategy({
     return done(null, profile._json);
   }
 ));
+
+// passport-facebook 설정
+passport.use(new FacebookStrategy({
+    clientID: '1831517760493063',
+    clientSecret: 'c114f7f7a17050286f8e9a1b05262e4b',
+    callbackURL: "http://localhost:9070/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    // 사용자의 정보는 profile에 들어있다.
+    console.log('profile', profile._json);
+    return cb(null, profile._json);
+  }
+));
+
+// passport-naver 설정
+passport.use(new NaverStrategy({
+        clientID: 's65ayNOzdLQkAVJyVG09',
+        clientSecret: 'qOZX2ptGmT',
+        callbackURL: "http://localhost:9070/auth/naver/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+      // 사용자의 정보는 profile에 들어있다.
+      console.log('profile', profile._json);
+      return done(null, profile._json);
+    }
+));
+
 passport.serializeUser(function(user, done) {
     done(null, user);
 });
@@ -29,8 +58,23 @@ passport.deserializeUser(function(obj, done) {
 });
 
 app.use(passport.initialize());
+// kakao 인증처리
 app.get('/auth/kakao', passport.authenticate('kakao'));
 app.get('/auth/kakao/callback', passport.authenticate('kakao', {
+  successRedirect: 'http://localhost:8080/code', // 성공하면 /code으로 가도록
+  failureRedirect: 'http://localhost:8080/login'
+}));
+
+// facebook 인증처리
+app.get('/auth/facebook', passport.authenticate('facebook'));
+app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+  successRedirect: 'http://localhost:8080/code', // 성공하면 /code으로 가도록
+  failureRedirect: 'http://localhost:8080/login'
+}));
+
+// naver 인증처리
+app.get('/auth/naver', passport.authenticate('naver'));
+app.get('/auth/naver/callback', passport.authenticate('naver', {
   successRedirect: 'http://localhost:8080/code', // 성공하면 /code으로 가도록
   failureRedirect: 'http://localhost:8080/login'
 }));
