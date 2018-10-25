982471d2e6f96da53174c949ce038233a8ea0ede
In joon. Gong <ijgong@nglp.kr>
2018-10-25 16:48:34 +0900

    mongoDB connect test remove MongoDB 적용 passport-local 적용

--- temp_authentication.js
+++ temp_authentication.js
@@ -1,6 +1,7 @@
 // TODO :: 20181023. 임시로 passport 처리로직 작성. 이후 로직 재개발 필요
 const express = require('express');
 var app = express();
+app.use(express.json());  // POST request 처리
 
 var host_ip = process.env.NODE_ENV === 'development' ? 'localhost' : '35.200.103.250';
 console.log(host_ip);
@@ -9,8 +10,14 @@ console.log(host_ip);
 var cors = require('cors');
 app.use(cors());
 
+// MongoDB 설정
+const MongoClient = require('mongodb').MongoClient;
+// const mongoUri = 'mongodb+srv://gijoona:mongodb77@cluster-quester-euzkr.gcp.mongodb.net/test?authSource=test&w=1';
+const mongoUri = 'mongodb+srv://gijoona:mongodb77@cluster-quester-euzkr.gcp.mongodb.net/test';
+
 // passport 설정
 var passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
     KakaoStrategy = require('passport-kakao').Strategy,
     FacebookStrategy = require('passport-facebook').Strategy,
     NaverStrategy = require('passport-naver').Strategy;
@@ -37,6 +44,28 @@ custom profile {
 	}
 }
 */
+// passport-local 설정
+passport.use(new LocalStrategy(function(username, password, done){
+  let profile = {};
+  console.log(username, password);
+  MongoClient.connect(mongoUri, function(err, client) {
+     if(err) {
+          console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+     }
+     console.log('Connected...');
+     let collection = client.db('test').collection('devices');
+
+     collection.find({id: username, password: password, provider: 'local'}).toArray(function(err, results){
+       if(err) throw err;
+       if (results.length == 0) {
+         return done(null, false);
+       } else {
+         return done(null, results);
+       }
+       client.close();
+     });
+  });
+}));
 // passport-kakao 설정
 passport.use(new KakaoStrategy({
     clientID : 'f8c9331888d39bc7d4a20aae640ec817',
@@ -47,6 +76,7 @@ passport.use(new KakaoStrategy({
     // 사용자의 정보는 profile에 들어있다.
     // _json.properties에 들어있는 데이터를 _json으로 한단계 올린다.
     Object.assign(profile._json, profile._json.properties);
+    save(profile);
     return done(null, profile);
   }
 ));
@@ -59,6 +89,7 @@ passport.use(new FacebookStrategy({
   },
   function(accessToken, refreshToken, profile, cb) {
     // 사용자의 정보는 profile에 들어있다.
+    save(profile);
     return cb(null, profile);
   }
 ));
@@ -71,6 +102,7 @@ passport.use(new NaverStrategy({
     },
     function(accessToken, refreshToken, profile, done) {
       // 사용자의 정보는 profile에 들어있다.
+      save(profile);
       return done(null, profile);
     }
 ));
@@ -83,6 +115,16 @@ passport.deserializeUser(function(obj, done) {
 });
 
 app.use(passport.initialize());
+// 회원가입
+app.get('/auth/join', function(req, res){
+  res.send('join');
+});
+
+// local 인증처리
+app.post('/auth/local', passport.authenticate('local', { failureRedirect: `http://${host_ip}:8080/login` }), function(req, res) {
+  res.redirect(`http://${host_ip}:8000/code`);
+});
+
 // kakao 인증처리
 app.get('/auth/kakao', passport.authenticate('kakao'));
 app.get('/auth/kakao/callback', passport.authenticate('kakao', {
@@ -107,3 +149,21 @@ app.get('/auth/naver/callback', passport.authenticate('naver', {
 app.listen(9070, function () {
   console.log(`authentication server listen host ${host_ip} port 9070`);
 });
+
+function save (profile) {
+  MongoClient.connect(mongoUri, function(err, client) {
+     if(err) {
+          console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+     }
+     console.log('Connected...');
+     let collection = client.db('test').collection('devices');
+     // const collection = client.db("test").collection("devices").insertOne(profile);
+     collection.find({id: profile.id, provider: profile.provider}).toArray(function(err, results){
+       if(err) throw err;
+       if (results.length == 0) {
+         collection.insertOne(profile);
+       }
+       client.close();
+     });
+  });
+}
