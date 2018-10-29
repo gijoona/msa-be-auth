8b8577d7b4d9670f9d0953860db5ae1752155239
In joon. Gong <ijgong@nglp.kr>
2018-10-29 17:31:19 +0900

    로그인 후 이동 페이지변경 code --> main

--- temp_authentication.js
+++ temp_authentication.js
@@ -122,27 +122,27 @@ app.get('/auth/join', function(req, res){
 
 // local 인증처리
 app.post('/auth/local', passport.authenticate('local', { failureRedirect: `http://${host_ip}:8080/login` }), function(req, res) {
-  res.redirect(`http://${host_ip}:8080/code`);
+  res.redirect(`http://${host_ip}:8080/main`);
 });
 
 // kakao 인증처리
 app.get('/auth/kakao', passport.authenticate('kakao'));
 app.get('/auth/kakao/callback', passport.authenticate('kakao', {
-  successRedirect: `http://${host_ip}:8080/code`, // 성공하면 /code으로 가도록
+  successRedirect: `http://${host_ip}:8080/main`, // 성공하면 /code으로 가도록
   failureRedirect: `http://${host_ip}:8080/login`
 }));
 
 // facebook 인증처리
 app.get('/auth/facebook', passport.authenticate('facebook'));
 app.get('/auth/facebook/callback', passport.authenticate('facebook', {
-  successRedirect: `http://${host_ip}:8080/code`, // 성공하면 /code으로 가도록
+  successRedirect: `http://${host_ip}:8080/main`, // 성공하면 /code으로 가도록
   failureRedirect: `http://${host_ip}:8080/login`
 }));
 
 // naver 인증처리
 app.get('/auth/naver', passport.authenticate('naver'));
 app.get('/auth/naver/callback', passport.authenticate('naver', {
-  successRedirect: `http://${host_ip}:8080/code`, // 성공하면 /code으로 가도록
+  successRedirect: `http://${host_ip}:8080/main`, // 성공하면 /code으로 가도록
   failureRedirect: `http://${host_ip}:8080/login`
 }));
 
